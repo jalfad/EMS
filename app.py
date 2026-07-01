1c94c660715aa1de253cd9c3a86b7a5bd278ae10
jalfad <jalfad@M0014>
2026-07-01 16:48:05 +0800

    modified edit

--- app.py
+++ app.py
@@ -170,11 +170,7 @@ def home():
             per_page=5
         )
         
-    for employee in employees.items:
-        print("==========================")
-        print("Employee No:", employee.employee_no)
-        print("Photo Source:", employee.photo_source)
-        print("Photo:", employee.photo) 
+
 
     return render_template(
         'index.html',
